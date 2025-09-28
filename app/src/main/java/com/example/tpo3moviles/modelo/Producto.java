9b18311b05008b3bcd5b69cf4281763aa8e7994e
candelagodoy <candegg13@gmail.com>
2025-09-28 16:10:09 -0300

    cambio

--- app/src/main/java/com/example/tpo3moviles/modelo/Producto.java
+++ app/src/main/java/com/example/tpo3moviles/modelo/Producto.java
@@ -1,6 +1,27 @@
 package com.example.tpo3moviles.modelo;
 
 public class Producto {
-    public Producto() {
+    private String nombre;
+    private String descripcion;
+
+    public Producto(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }
